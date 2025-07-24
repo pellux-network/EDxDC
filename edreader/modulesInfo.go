09b4e2755f96cdafd339c79cda9695a45aff4204
Pellux Network <69098091+pellux-network@users.noreply.github.com>
2025-07-23 18:21:24 -0600

    fix: added support for mkii cargo racks

--- edreader/modulesInfo.go
+++ edreader/modulesInfo.go
@@ -22,7 +22,7 @@ type ModulesLine struct {
 }
 
 var currentModules ModulesInfo
-var currentCargoCapacity int // NEW: updated from Loadout event
+var currentCargoCapacity int
 
 func handleModulesInfoFile(file string) {
 	data, err := os.ReadFile(file)
@@ -39,27 +39,49 @@ func ModulesInfoCargoCapacity() int {
 	}
 	cargoCapacity := 0
 
-	for _, line := range currentModules.Modules {
+	for i, line := range currentModules.Modules {
+		log.Debug().
+			Int("index", i).
+			Str("slot", line.Slot).
+			Str("item", line.Item).
+			Msg("Processing module line")
+
 		switch line.Item {
 		case "int_cargorack_size1_class1":
 			cargoCapacity += 2
+			log.Debug().Int("added", 2).Int("total", cargoCapacity).Msg("Matched size1 cargo rack")
 		case "int_cargorack_size2_class1":
 			cargoCapacity += 4
+			log.Debug().Int("added", 4).Int("total", cargoCapacity).Msg("Matched size2 cargo rack")
 		case "int_cargorack_size3_class1":
 			cargoCapacity += 8
+			log.Debug().Int("added", 8).Int("total", cargoCapacity).Msg("Matched size3 cargo rack")
 		case "int_cargorack_size4_class1":
 			cargoCapacity += 16
+			log.Debug().Int("added", 16).Int("total", cargoCapacity).Msg("Matched size4 cargo rack")
 		case "int_cargorack_size5_class1":
 			cargoCapacity += 32
+			log.Debug().Int("added", 32).Int("total", cargoCapacity).Msg("Matched size5 cargo rack")
 		case "int_cargorack_size6_class1":
 			cargoCapacity += 64
+			log.Debug().Int("added", 64).Int("total", cargoCapacity).Msg("Matched size6 cargo rack")
 		case "int_cargorack_size7_class1":
 			cargoCapacity += 128
+			log.Debug().Int("added", 128).Int("total", cargoCapacity).Msg("Matched size7 cargo rack")
+		case "int_largecargorack_size7_class1":
+			cargoCapacity += 192
+			log.Debug().Int("added", 192).Int("total", cargoCapacity).Msg("Matched large size7 cargo rack")
 		case "int_cargorack_size8_class1":
 			cargoCapacity += 256
+			log.Debug().Int("added", 256).Int("total", cargoCapacity).Msg("Matched size8 cargo rack")
+		case "int_largecargorack_size8_class1":
+			cargoCapacity += 384
+			log.Debug().Int("added", 384).Int("total", cargoCapacity).Msg("Matched large size8 cargo rack")
+		default:
+			log.Trace().Str("item", line.Item).Msg("Module not counted for cargo capacity")
 		}
 	}
 
+	log.Info().Int("final_cargo_capacity", cargoCapacity).Msg("Calculated total cargo capacity")
 	return cargoCapacity
-
 }
